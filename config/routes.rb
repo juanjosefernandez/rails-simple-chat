fc5018c6d2a90a747bd02b0011b40ff1544ab56a
Juan Jose Fernandez <fernandez.juan.j@gmail.com>
2020-12-06 16:53:45 -0500

    Adds Rooms and Messages, alongwith Authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root controller: :rooms, action: :index
+
+  resources :room_messages
+  resources :rooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
