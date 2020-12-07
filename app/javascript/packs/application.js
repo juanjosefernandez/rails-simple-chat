056293c8da365b9e1ecd100dba6e8dc8bab9fc5c
Juan Jose Fernandez <fernandez.juan.j@gmail.com>
2020-12-06 20:20:08 -0500

    Creates and displays room messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("custom/room")
+
+
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
